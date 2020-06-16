7a7399f0c82467732ccaccb63e4685571389c7e1
André Oliveira <badecoandre1999@gmail.com>
2020-06-16 01:16:02 -0300

    publish website

--- models/database/connection.js
+++ models/database/connection.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const connection = new Sequelize('lachapa', 'adminlachapa', '12345678a', {
     host: 'mysql669.umbler.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: 41890
 });
 
 module.exports = connection;
